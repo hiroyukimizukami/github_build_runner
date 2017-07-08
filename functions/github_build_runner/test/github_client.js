64d082eaf27bfca351e9d0056b271182b1563fde
hiroyukimizukami <majestaaa@gmail.com>
2017-07-08 15:06:49 +0900

    Add test

--- functions/github_build_runner/test/github_client.js
+++ functions/github_build_runner/test/github_client.js
@@ -1,6 +1,24 @@
 var GithubClient = require('../github_client')
+var MockHttps = require('./https_mock')
 var assert = require('assert')
+var util = require('util')
 
 describe('getPullRequest', () => {
-    it('pass arguemtns correctly', () => assert.equal(true, true) )
+    var number = 2
+    var https = MockHttps.create()
+    var repo = 'sample_repo'
+    var username = 'sample_user'
+    var token = '12345'
+    var githubClient = GithubClient.create(repo, username, token)
+    githubClient.https = https
+    githubClient.getPullRequest(number, () => {}, () => {})
+
+    console.log(https.request.param)
+    it('port', () => assert.equal(https._request.param.port, 443))
+    it('host', () => assert.equal(https._request.param.host, 'api.github.com'))
+    it('method', () => assert.equal(https._request.param.method, 'GET'))
+    it('path', () => {
+        var expectation = util.format('/repos/%s/%s/pulls/%s', username, repo, number)
+        assert.equal(https._request.param.path, expectation)
+    })
 })
