837b1693da63cbcff34f794aae0bceb89e734666
hiroyukimizukami <majestaaa@gmail.com>
2017-07-09 02:28:18 +0900

    fix module name

--- functions/github/main.js
+++ functions/github/main.js
@@ -8,7 +8,7 @@ exports.call = (e, context, callback) => {
     console.log(e)
     let event = IssueCommentEvent.create(e)
     let github = GithubClient.create(config.repo, config.username, process.env.GITHUB_TOKEN)
-    let circleci = GithubClient.create(config.repo, config.username, process.env.CIRCLECI__TOKEN)
+    let circleci = CircleCIClient.create(config.repo, config.username, process.env.CIRCLECI__TOKEN)
 
     if (!HookRules.shouldBuild(e)) {
         callback(null, { status: 0 })
